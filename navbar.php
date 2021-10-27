8d31413f8b54b9e2d0c88be90c065e51037f7a56
mr.mb7 <mr.mb7@mail.ru>
2021-10-27 17:34:30 +0300

    Разбил данные на файлы

---
+++ navbar.php
@@ -0,0 +1,33 @@
+<!-- Navbar -->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+    <div class="container-fluid">
+        <div class="bd-brand-item w-50 p-1">
+            <div class="h2">Рационы питания</div>
+        </div>
+
+
+        <a class="navbar-brand" href="#"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Главная страница </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">О компании</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Отзывы</a>
+                </li>
+            </ul>
+        </div>
+        <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+    </div>
+</nav>
+</div>
